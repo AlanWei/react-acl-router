1c3b9d736d066b9958c34b2cb5dc1fce1c691456
Marcel Koch <kontakt@marcelkoch.net>
2018-07-27 12:24:23 +0200

    Add proptypes to Link

--- src/Link.jsx
+++ src/Link.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { intersection } from 'lodash';
 import AclRouterContext from './AclRouterContext';
@@ -34,5 +35,9 @@ const AclRouterLink = props => (
     }}
   </AclRouterContext.Consumer>
 );
+
+AclRouterLink.propTypes = {
+  to: PropTypes.string.isRequired,
+};
 
 export default AclRouterLink;
